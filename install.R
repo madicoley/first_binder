38ed857233f4996a10e09d7f2dca2d0ae48c2001
madicoley <96292739+madicoley@users.noreply.github.com>
2021-12-17 11:39:08 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c("tidyverse", "Hmisc", "performance", "MASS', "car", "olsrr", "patchwork"))
+install.packages(c("tidyverse", "Hmisc", "performance", "MASS", "car", "olsrr", "patchwork"))
